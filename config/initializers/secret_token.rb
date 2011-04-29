006f7bf0ae031415d92311b040cb491932d21605
saul <saulsanfrancisco@gmail.com>
2011-04-29 02:54:26 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectiverse::Application.config.secret_token = '479e9911f7970b139b19a28ef791bedd4472ec0f69ea6029479a9201ffe8e3eca938924156038be3a2e7bf114a7fd7bb80fb37a8ef312a30b9392b7cf3371559'
