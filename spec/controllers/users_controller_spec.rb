ef7492d1c8ad30d709526b42af15e08e23568045
saul <saulsanfrancisco@gmail.com>
2011-06-11 15:03:09 -0500

    new

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -66,6 +66,30 @@ describe "POST 'create'" do
     post :create, :user => @attr
     response.should render_template('new')
    end
+  end 
+
+ 
+  describe "success" do
+
+   before(:each) do
+    @attr = { :name => "New User", :email => "user@example.com", :password => "foobar", :password_confirmation => "foobar" }
+   end
+
+   it "should create a user" do
+    lambda do
+     post :create, "user" => @attr
+    end.should change(User, :count) .by(1)
+   end
+
+   it "should redirect to the user show page" do 
+    post :create, :user => @attr
+    response.should redirect_to(user_path(assigns(:user)))
+   end
+
+   it "should have a welcome message" do 
+    post :create, :user => @attr
+    flash[:success].should =~ /welcome to Vera Maurina Press Micro-Text Exchange/i 
+   end
   end
  end
 end
