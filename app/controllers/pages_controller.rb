b11fa4b0dd930c86f1930f457a61328da9285e48
saul <saulsanfrancisco@gmail.com>
2011-06-17 22:34:11 -0400

    Added user microposts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   
   def home
     @title = "Home"
+    if signed_in?
+     @micropost = Micropost.new
+     @feed_items = current_user.feed.paginate(:page => params[:page])
+   end
   end
 
   def contact
@@ -15,4 +19,5 @@ class PagesController < ApplicationController
   def help
     @title = "Help"
   end
+
 end
